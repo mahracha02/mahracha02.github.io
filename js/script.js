b85fa3cd489f784c548422dcbcec0649d33576f6
mahracha02 <abdelhakim22mahracha@gmail.com>
2024-04-07 05:21:17 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,73 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: "auto",
+    spaceBetween: 25,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination-cv",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+
+    },
+    
+    
+  });
+
+  var swiper = new Swiper(".slide-contente", {
+    slidesPerView: "auto",
+    spaceBetween: 25,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination-cv2",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+
+    },
+    
+    
+  });
+
+  var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next ",
+      prevEl: ".swiper-button-prev ",
+    },
+  });
+
+  var swiper = new Swiper(".slide-skills", {
+    slidesPerView: "auto",
+    spaceBetween: 25,
+    loop: true,
+    autoplay: {
+        delay: 2000, // Définissez la vitesse de défilement (en millisecondes)
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 25,
+    loop: true,
+    autoplay: {
+        delay: 2000, // Définissez la vitesse de défilement (en millisecondes)
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+  });
