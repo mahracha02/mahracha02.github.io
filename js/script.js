bc090da186e9361facb6c06ec34a75c0c4d79f52
mahracha02 <abdelhakim22mahracha@gmail.com>
2024-04-22 17:58:05 +0200

    update

--- js/script.js
+++ js/script.js
@@ -32,23 +32,9 @@ var swiper = new Swiper(".slide-content", {
     
   });
 
-  var swiper = new Swiper(".client-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next ",
-      prevEl: ".swiper-button-prev ",
-    },
-  });
-
   var swiper = new Swiper(".slide-skills", {
     slidesPerView: "auto",
-    spaceBetween: 25,
+    spaceBetween:30 ,
     loop: true,
     autoplay: {
         delay: 2000, // Définissez la vitesse de défilement (en millisecondes)
@@ -69,5 +55,19 @@ var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    spaceBetween: 25,
+    loop: true,
+    autoplay: {
+        delay: 2000, // Définissez la vitesse de défilement (en millisecondes)
+    },
+    pagination: {
+        el: ".swiper-pagination-photo",
+        clickable: true,
+        
     },
   });
