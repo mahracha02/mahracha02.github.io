471ef691f4f8b17bd9c3edfa320f7eb7d9fdbcc1
mahracha02 <abdelhakim22mahracha@gmail.com>
2024-05-31 16:52:48 +0200

    update projet analim

--- js/script.js
+++ js/script.js
@@ -87,5 +87,30 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("openModalBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on the button, open the modal
+  btn.onclick = function() {
+      modal.style.display = "flex";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
 
   
